9a8e8dd4750e0585ad2695f0755bd512e96d895b
DanielASBatista <daniel_alves.santos@hotmail.com>
2025-09-30 21:57:21 -0300

    Aula 10 - Relacionamento na API.

---
+++ Data/DataContext.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RpgApi.Models;
+using RpgApi.Models.Enuns;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace RpgApi.Data
+{
+    public class DataContext : DbContext
+    {
+        //ctor
+        public DataContext 
+        (DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+        //prop 
+        public DbSet<Personagem> TB_PERSONAGENS { get; set; }
+        public DbSet<Arma> TB_ARMAS { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Personagem>().ToTable("TB_PERSONAGENS");
+            modelBuilder.Entity<Arma>().ToTable("TB_ARMAS");
+
+            modelBuilder.Entity<Personagem>().HasData
+            (                
+                new Personagem() { Id = 1, Nome = "Frodo", PontosVida = 100, Forca = 17, Defesa = 23, Inteligencia = 33, Classe = ClasseEnum.Cavaleiro, UsuarioId =1},
+                new Personagem() { Id = 2, Nome = "Sam", PontosVida = 100, Forca = 15, Defesa = 25, Inteligencia = 30, Classe = ClasseEnum.Cavaleiro, UsuarioId =1},
+                new Personagem() { Id = 3, Nome = "Galadriel", PontosVida = 100, Forca = 18, Defesa = 21, Inteligencia = 35, Classe = ClasseEnum.Clerigo, UsuarioId =1},
+                new Personagem() { Id = 4, Nome = "Gandalf", PontosVida = 100, Forca = 18, Defesa = 18, Inteligencia = 37, Classe = ClasseEnum.Mago, UsuarioId =1},
+                new Personagem() { Id = 5, Nome = "Hobbit", PontosVida = 100, Forca = 20, Defesa = 17, Inteligencia = 31, Classe = ClasseEnum.Cavaleiro, UsuarioId =1},
+                new Personagem() { Id = 6, Nome = "Celeborn", PontosVida = 100, Forca = 21, Defesa = 13, Inteligencia = 34, Classe = ClasseEnum.Clerigo, UsuarioId =1},
+                new Personagem() { Id = 7, Nome = "Radagast", PontosVida = 100, Forca = 25, Defesa = 11, Inteligencia = 35, Classe = ClasseEnum.Mago, UsuarioId =1}
+            );
+
+             modelBuilder.Entity<Arma>().HasData(
+                new Arma() { Id = 1, Nome = "Anti-Material Rifle", Dano = 70},
+                new Arma() { Id = 2, Nome = "Umbra", Dano = 50},
+                new Arma() { Id = 3, Nome = "Salame", Dano = 100},
+                new Arma() { Id = 4, Nome = "Blade of Chaos", Dano = 35},
+                new Arma() { Id = 5, Nome = "Marreta", Dano = 55 },
+                new Arma() { Id = 6, Nome = "AK-43", Dano = 70 },
+                new Arma() { Id = 7, Nome = "El Machete",Dano = 80 }
+            );
+
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.ConfigureWarnings(warnings => warnings
+                .Ignore(RelationalEventId.PendingModelChangesWarning));
+        }
+
+
+
+
+
+    }
+}
